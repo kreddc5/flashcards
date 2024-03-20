1db9592289115745239cde670f3fcb6f7e885437
kredd <kredd@vailsys.com>
2024-03-19 19:27:08 -0500

    added cookie reader, added button to clear cookie that redirects you to hello page to input another name

--- app.js
+++ app.js
@@ -9,33 +9,45 @@ app.set('view engine', 'pug')
 
 
 //will display this list of arrays on the card route
-const colors = [
-    'red',
-    'orange',
-    'yellow',
-    'green',
-    'blue',
-    'purple'
-  ];
+// const colors = [
+//     'red',
+//     'orange',
+//     'yellow',
+//     'green',
+//     'blue',
+//     'purple'
+//   ];
 
 app.get('/', (req, res) => { 
     const name = req.cookies.username; 
-    res.render('index', {name});
-}); 
+    if (name) {
+        res.render('index', {name})
+    } else {
+        res.redirect('/hello');
+    }
 
+}); 
 app.get('/cards', (req, res) => { 
     res.render('card', {prompt: 'Who is buried in grants tomb', colors});
 }); 
-
 app.get('/hello', (req, res) => { 
+    const name = req.cookies.username; 
+    if (name) {
+        res.redirect('/')
+    } else {
+        res.render('hello');
+    }
     //when users get to the hello route hello will be rendered 
     res.render('hello');
 }); 
-
 app.post('/hello', (req, res) => { 
     res.cookie('username', req.body.username)
     //when users get to the hello route hello will be rendered 
     res.redirect('/');
+}); 
+app.post('/goodbye', (req, res) => { 
+    res.clearCookie('username');
+    res.redirect('/hello');
 }); 
 
 app.listen(3000, () => {
